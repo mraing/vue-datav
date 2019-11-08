c7abe34395525f57a40685451c3c8a0e7513465c
mraing <910408228@qq.com>
2019-11-08 09:58:20 +0800

    datav finish
    可视化数据展示

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 可视化数据
+import dataV from '@jiaminghi/data-view'
 Vue.config.productionTip = false
+
+Vue.use(dataV)
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Datav from '@/components/Datav'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Datav',
+      component: Datav
     }
   ]
 })
